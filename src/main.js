549c9da84cd46da6366b4dce0a3a31cbe45c60c1
Lucas PARMENTIER <johndoe@example.com>
2018-08-17 22:02:11 +0200

    Language selector

--- src/main.js
+++ src/main.js
@@ -4,36 +4,48 @@ import _ from 'lodash';
 const Hypher = require('hypher');
 const english = require('hyphenation.en-us');
 const french = require('hyphenation.fr');
-const h = new Hypher(english);
-const h1 = new Hypher(french);
+const en = new Hypher(english);
+const fr = new Hypher(french);
 
 
 const baseForm = Vue.component('base-form', {
   template: '<div>\
-  <p>Saisissez ici votre texte</p>\
-  <textarea class="hyphenate" id="textToCheck" v-bind:value="textToCheck" v-on:input="$emit(\'input\', $event.target.value)" type="text"></textarea>\
+  <h1>Saisissez ici votre texte</h1>\
+  <textarea id="textToCheck" v-bind:value="textToCheck" v-on:input="$emit(\'input\', $event.target.value)"></textarea>\
   </div>',
   props: ['textToCheck'],
-})
+});
+
+Vue.filter('fixed', (value, decimals) => {
+  if (!value) return 0
+  return value.toFixed(decimals);
+});
 
 const app = new Vue({
   el: '#app',
   data: {
-    textToCheck: 'placeholder',
+    textToCheck: '',
+    language: 'english',
     wordCount: 0,
     sentencesCount: 0,
     syllabeCount: 0,
-    fleschReadingEase: 0,
+    fkReadingEase: 0, // fk for Flesch–Kincaid
+    fkGradeLevel: 0,
   },
-  components: {baseForm: baseForm},
+  components: {
+    baseForm: baseForm,
+  },
   methods: {
     checkText: function() {
       this.wordCount = wordCount(this.textToCheck);
       this.sentencesCount = sentencesCount(this.textToCheck);
-      this.syllabeCount = syllabeCount(this.textToCheck);
-      this.fleschReadingEase = 206.835 - 1.015 * (this.wordCount / this.sentencesCount);
-      this.fleschReadingEase -= 84.6 * (this.syllabeCount / this.wordCount);
-      console.log(this.fleschReadingEase);
+      this.syllabeCount = syllabeCount(this.textToCheck, this.language);
+
+      this.fkReadingEase = 206.835 - 1.015 * (this.wordCount / this.sentencesCount);
+      this.fkReadingEase -= 84.6 * (this.syllabeCount / this.wordCount);
+
+      this.fkGradeLevel = 0.39 * (this.wordCount / this.sentencesCount);
+      this.fkGradeLevel += (11.8 * (this.syllabeCount / this.wordCount)) - 15.59;
     },
   },
 });
@@ -47,14 +59,15 @@ function sentencesCount(str) {
   // will count abreviations as sentences (Dr. Sgt. etc.)
   str = str.replace(/(\[\w+\])/g, ''); // Supresses words between brackets (ex: wikipedia references)
   str = str.replace(/(\.{3})/g, '$1|'); // Matches ...
-  str = str.replace(/(.[.?!](\s|$))/g, "$1|"); // matches any character followed by a phrase end
+  str = str.replace(/(.[.?!](\s|$))/g, "$1|"); // matches any character followed by a phrase ending character
   return str.split("|").length; // split along the pipes we added
 }
 
-function syllabeCount(str) {
+function syllabeCount(str, language) {
   let syllabeCount = 0;
   _.each(str.trim().split(/\s+/), (word) => {
-    syllabeCount += h.hyphenate(word).length;
+    if (language === 'french') syllabeCount += fr.hyphenate(word).length;
+    else syllabeCount += en.hyphenate(word).length;
   });
   return syllabeCount;
 }
